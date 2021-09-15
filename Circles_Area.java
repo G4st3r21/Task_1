fd60211e41107a49f7a8efc6217df24fb4de9f18
G4st3r21 <70285259+G4st3r21@users.noreply.github.com>
2021-09-15 16:13:22 +0300

    Add files via upload

--- Circles_Area.java
+++ Circles_Area.java
@@ -7,14 +7,32 @@ public class Circles_Area {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
 
+        double leg = readNum();
+
+        checkNum(leg);
+
+        double area = count(leg);
+
+        System.out.printf("Area of a circle circumscribed around an equilateral triangle: %1$.3f", area);
+    }
+
+    static double readNum() {
+        System.out.print("Enter leg length: ");
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите длину катета: ");
-        double a = scanner.nextDouble();
+        return scanner.nextDouble();
+    }
 
-        double r = (Math.sqrt(2 * a * a) / 2);
-        double p = Math.PI * r * r;
+    static void checkNum(double leg) {
+        if (leg < 0) {
+            System.out.println("Invalid input format");
+            System.exit(0);
+        }
+    }
 
-        System.out.printf("Площадь окружности, описанной вокруг прямоугольного равобедренного треугольника: %1$.3f", p);
+    static double count(double leg) {
+        double r = (Math.sqrt(2 * leg * leg) / 2);
+
+        return Math.PI * r * r;
     }
 }
